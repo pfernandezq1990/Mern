3b5c1d8143ce9093e1521cf7c0d3ba53b8fad6bc
pfernandezq1990 <pfernandezq1990@gmail.com>
2021-09-08 20:43:25 -0400

    last update

--- challenge/src/redux/actions/userActions.ts
+++
@@ -1,8 +0,0 @@
-import { SET_USER, SET_ERRORS, LOADING_UI, CLEAR_ERRORS, SET_UNAUTHENTICATED, LOADING_USER } from '../types';
-
-export const loginUser = ( userData: any, history: any ) => (dispatch: any ) => {
-    dispatch({ type: LOADING_UI })
-
-    const token = 'login successfully';
-    localStorage.setItem('token', token);
-}
--- my-app/src/components/filmes/Filmes.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Filmes = () => {
-  return <div>Filmes</div>;
-};
-
-export default Filmes;
